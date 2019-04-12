000c32d265caadca5e825ae1576be4df043a7645
Shishir Madari <avenuecode@shishir-mbp.local.gap.com>
2019-04-12 15:05:12 -0700

    implementing header

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Header from './components/header/header';
+
 import './App.css';
 
 
 class Home extends Component {
   render() {
     return (
-      <div className="App wrapper">
-        {/* <header className="App-header">
-        </header> */}
-        <div class="section-one parallax image-one fadein" />
-        <div>
-          hello
+      <React.Fragment>
+        <Header />
+        <div className="App wrapper">
+          <div className="grid-container">
+            <div className="section-one parallax image-one fadein" />
+            <div className="text">
+              hello
+            </div>
+          </div>
+          {/* <div className="grid-container">
+            <div class="section static">
+              <h1>Next Section</h1>
+            </div>
+          </div> */}
         </div>
-        <div class="section static">
-          <h1>Next Section</h1>
-        </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
